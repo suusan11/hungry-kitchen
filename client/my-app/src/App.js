75c60567c1e8d681f46067258bc4d802680eb78b
suusan11 <gawow11@gmail.com>
2018-12-07 17:00:05 -0800

    undo app files

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
-
-//import scss folder
-import './scss/style.scss';
-
-//import each component
-import Header from "./components/headerComponent/header";
-import Main from "./components/mainComponent/main";
-import Footer from "./components/footerComponent/footer";
-
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Header />
-                <Main />
-                <Footer />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
