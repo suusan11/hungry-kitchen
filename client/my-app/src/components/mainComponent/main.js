f3c9ecc30ac26cf399be6fb5256e0cbfa4c84c1f
suusan11 <gawow11@gmail.com>
2018-12-10 09:45:44 -0800

    created main.scss

--- client/my-app/src/components/mainComponent/main.js
+++ client/my-app/src/components/mainComponent/main.js
@@ -3,9 +3,31 @@ import React, { Component } from 'react';
 class Main extends Component {
     render() {
         return (
-            <div>
-                <img src="" alt=""/>
-                <h2>title</h2>
+            <div className="cards">
+                <div className="card">
+                    <img src="" alt=""/>
+                    <h2>title</h2>
+                </div>
+                <div className="card">
+                    <img src="" alt=""/>
+                    <h2>title</h2>
+                </div>
+                <div className="card">
+                    <img src="" alt=""/>
+                    <h2>title</h2>
+                </div>
+                <div className="card">
+                    <img src="" alt=""/>
+                    <h2>title</h2>
+                </div>
+                <div className="card">
+                    <img src="" alt=""/>
+                    <h2>title</h2>
+                </div>
+                <div className="card">
+                    <img src="" alt=""/>
+                    <h2>title</h2>
+                </div>
             </div>
         );
     }
