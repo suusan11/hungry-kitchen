a797453ad94549d8f8e3db9654d2adfa6a1c9ba4
suusan11 <gawow11@gmail.com>
2018-12-10 09:57:12 -0800

    added event to get input value

--- client/my-app/src/components/headerComponent/header.js
+++ client/my-app/src/components/headerComponent/header.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+
+    onInputChangeHandler = (event) => {
+        console.log(event.target.value);
+    }
+
     render() {
         return (
             <header>
                 <div className="header__flex">
                     <div className='logo'>Hungry-Kitchen</div>
-                    <input className='searchBox' type="text"/>
+                    <input className='searchBox' type="text" onChange={this.onInputChangeHandler}/>
                 </div>
             </header>
         );
