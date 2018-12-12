e38d3ab7fb1d0793df72f0ea96722c73a01c5284
suusan11 <gawow11@gmail.com>
2018-12-12 11:30:22 -0800

    added img

--- client/my-app/src/components/mainComponent/card.js
+++ client/my-app/src/components/mainComponent/card.js
@@ -1,11 +1,22 @@
 import React, { Component }from 'react';
+import recipe from '../../images/recipe1.JPG';
 
 class Card extends Component {
     render(){
         return(
-            <div className="card">
-                <img src="" alt=""/>
-                <h2>title</h2>
+            <div className="cards">
+                <div className="card">
+                    <img src={recipe} alt=""/>
+                    <h2>title</h2>
+                </div>
+                <div className="card">
+                    <img src={recipe} alt=""/>
+                    <h2>title</h2>
+                </div>
+                <div className="card">
+                    <img src={recipe} alt=""/>
+                    <h2>title</h2>
+                </div>
             </div>
         );
     }
