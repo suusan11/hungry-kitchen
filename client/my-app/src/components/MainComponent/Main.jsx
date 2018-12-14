e3212fb9a6d61f64ca2ecacf864eaab19f884af3
suusan11 <gawow11@gmail.com>
2018-12-14 12:16:38 -0800

    fixed margin

--- client/my-app/src/components/MainComponent/Main.jsx
+++ client/my-app/src/components/MainComponent/Main.jsx
@@ -2,22 +2,26 @@ import React, { Component } from 'react';
 import { recipeItems } from '../../constants/recipeItems';
 import RecipeItem from "./RecipeItem";
 import './_main.scss';
+import Container from "../../Container/Container";
+import '../../Container/Container.scss';
 
 class Main extends Component {
     render() {
         return (
-            <div className="main">
-                <h2>New Recipe</h2>
-                <div className="recipeItem">
-                {recipeItems.map((recipeItem) => (
-                    <RecipeItem
-                        key = {recipeItem.title}
-                        imageUrl = {recipeItem.imageUrl}
-                        title = {recipeItem.title}
-                    />
-                ))}
+            <Container>
+                <div className="main">
+                    <h2>New Recipe</h2>
+                    <div className="recipeItem">
+                    {recipeItems.map((recipeItem) => (
+                        <RecipeItem
+                            key = {recipeItem.title}
+                            imageUrl = {recipeItem.imageUrl}
+                            title = {recipeItem.title}
+                        />
+                    ))}
+                    </div>
                 </div>
-            </div>
+            </Container>
         );
     }
 }
